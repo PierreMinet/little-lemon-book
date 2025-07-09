8e34025d2540e66d17a899f680e3e18ea6d2a458
Pierre Minet <pierre.minet@outlook.fr>
2025-07-09 17:26:41 +0200

    Add base of CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
 function Header() {
     return (
         <header>
+            <div>
+                <h1>Little Lemon</h1>
+                <h2>Chicago</h2>
+                <p>We are a family owned Mediterranean restaurant, focused on traditional recipers served with a modern twist.</p>
+            </div>
         </header>
     );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,19 +2,17 @@ import logo from '../Logo.svg';
 
 function Nav() {
     return (
-        <>
+        <nav>
             <img src={logo} alt="Logo" />
-            <nav>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Menu</a></li>
-                    <li><a href="#">Reservations</a></li>
-                    <li><a href="#">Order Online</a></li>
-                    <li><a href="#">Login</a></li>
-                </ul>
-            </nav>
-        </>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Menu</a></li>
+                <li><a href="#">Reservations</a></li>
+                <li><a href="#">Order Online</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+        </nav>
     );
 }
 
