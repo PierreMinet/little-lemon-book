b138f701ef3a46c0a9ff4fb33c9e2d262fec29cf
Pierre Minet <pierre.minet@outlook.fr>
2025-07-21 11:04:28 +0200

    Add menu for mobile device

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import logo from '../Logo.svg';
 import {Link} from 'react-router-dom';
 
 function Nav() {
+
+    const [navigation, setNavigation] = useState(false);
+
     return (
         <nav>
             <img src={logo} alt="Logo" />
-            <ul>
+            <button aria-label="Menu" className='button-normal section-category nav-burger' onClick={() => setNavigation(!navigation)}>Menu</button>
+            <ul className={navigation ? "active nav-menu" : "nav-menu"}>
                 <Link to="/" aria-label="Home page" className='lead-text'>Home</Link>
                 <Link aria-label="About" className='lead-text'>About</Link>
                 <Link aria-label="Menu" className='lead-text'>Menu</Link>
